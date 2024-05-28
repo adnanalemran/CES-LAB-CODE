6b860d96c20b9f5d00761c95dcdf2576e24ba543
adnanalemran <adnanalemranontor@gmail.com>
2024-05-28 22:30:32 +0600

    chore: Update package.json dependencies
    This commit updates the dependencies in the package.json file. It adds the "axios" package with version "^1.6.5" and updates the versions of "dotenv", "express", "mongodb", "mongoose", and "nodemon" packages.
    
    Note: This is a suggested commit message based on the code changes provided. Please modify it as needed to adhere to your repository's commit message conventions.

---
+++ backend/src/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
+const port = process.env.PORT || 5000;
+const axios = require("axios");
+require("dotenv").config();
+ 
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zq9jmri.mongodb.net/?retryWrites=true&w=majority
+`;
+-mongoose.connect(uri, { dbName: process.env.DB_NAME });
+
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+  console.error(`Error connecting to MongoDB: ${err}`);
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+//start product function
+
+app.use("/product", productHandler);
+
+//start user function
+
+app.use("/user", userHandler);
+app.use("/cost", costHandler);
+app.use("/card", cardHandler);
+app.use("/sell", sellHandler);
+app.use("/money", moneyHandler);
+// --------------------------------------localApi-------------------------------------------
+
+app.get("/", (req, res) => {
+  res.send("Database server is  connected");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
